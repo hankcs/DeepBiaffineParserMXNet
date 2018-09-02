fixed bug in arc softmax

models/biaffine_parser.py
@@ -206,7 +206,7 @@ class BiaffineParser(nn.Block):
 
         if not is_train:
             arc_probs = np.transpose(
-                np.reshape(nd.softmax(flat_arc_logits).asnumpy(), (seq_len, seq_len, batch_size), 'F'))
+                np.reshape(nd.softmax(flat_arc_logits, axis=0).asnumpy(), (seq_len, seq_len, batch_size), 'F'))
 
         _target_vec = nd.array(targets_1D if is_train else flatten_numpy(arc_preds.asnumpy())).reshape(
             seq_len * batch_size, 1)
@@ -238,12 +238,11 @@ class BiaffineParser(nn.Block):
 
         outputs = []
 
-        for msk, arc_prob, rel_prob, a in zip(np.transpose(mask), arc_probs, rel_probs, arc_preds.asnumpy().transpose([1, 0])):
+        for msk, arc_prob, rel_prob in zip(np.transpose(mask), arc_probs, rel_probs):
             # parse sentences one by one
             msk[0] = 1.
             sent_len = int(np.sum(msk))
             arc_pred = arc_argmax(arc_prob, sent_len, msk)
-            arc_pred = a.astype(int)
             rel_prob = rel_prob[np.arange(len(arc_pred)), arc_pred]
             rel_pred = rel_argmax(rel_prob, sent_len)
             outputs.append((arc_pred[1:sent_len], rel_pred[1:sent_len]))